cfe6a3d5845c7a3d5dc58b334d0cc0c5c7b93b27
zishancoder <zishanahmad.rk@gmail.com>
2023-02-27 10:24:47 +0530

    added some pattern problems

--- Patterns/P2.cpp
+++ Patterns/P2.cpp
@@ -1,7 +1,7 @@
 /*
-    *   *   *
-    *   *   *
-    *   *   *
+    1   1   1
+    2   2   2
+    3   3   3
 */
 
 #include<iostream>
@@ -10,14 +10,14 @@ using namespace std;
 int main(){
     int row=1,col=1,n;
     cin>>n;
+
     while(row<=n){
         col=1;
         while(col<=n){
-            cout<<"*";
+            cout<<row<<"\t";
             col++;
         }
         cout<<endl;
         row++;
     }
-
 }
